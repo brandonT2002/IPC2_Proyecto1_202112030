cb9d77076600fbece09713ff32c6c4bb460a980e
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-02-20 05:19:31 -0600

    function to clone the original matrix

--- Algorithm/Algorithm.py
+++ Algorithm/Algorithm.py
@@ -4,3 +4,6 @@ class Algorithm:
     def __init__(self,matrix):
         self.matrix = matrix
         self.clon = self.clone(self.matrix)
+
+    def clone(self,matrix : Matrix) -> Matrix:
+        return matrix.clone()
