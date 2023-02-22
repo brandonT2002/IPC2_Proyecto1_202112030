958723921e60905d61f813ffc328c12eadcefba5
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-02-22 17:40:18 -0600

    changing from linked list to sparse list

--- Matrix/HeaderList.py
+++ Matrix/HeaderList.py
@@ -7,9 +7,43 @@ class HeaderList:
 
     def insertNode(self,index):
         if self.first:
-            self.last.next = HeaderNode(index)
-            self.last.prev = self.last
-            self.last = self.last.next
+            if index < self.first.index:
+                self.first.prev = HeaderNode(index)
+                self.first.prev.next = self.first
+                self.first = self.first.prev
+            elif index > self.last.index:
+                self.last.next = HeaderNode(index)
+                self.last.next.prev = self.last
+                self.last = self.last.next
+            else:
+                current = self.first
+                while current.next:
+                    if index > current.index and index < current.next.index:
+                        temp = HeaderNode(index)
+                        temp.prev = current
+                        temp.next = current.next
+
+                        current.next.prev = temp
+                        current.next = temp
+                        return
+                    current = current.next
             return
         self.first = HeaderNode(index)
         self.last = self.first
+
+    def isHearIndex(self,index):
+        if self.first:
+            current = self.first
+            while current:
+                if current.index == index:
+                    return True
+                current = current.next
+            return False
+
+    def print(self):
+        current = self.first
+        w = ''
+        while current:
+            w += f'{current.index}'
+            current = current.next
+        print(w)
