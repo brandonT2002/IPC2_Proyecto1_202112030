e4b730f798db19db941e517a581164d2b0effd9f
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-02-21 22:51:17 -0600

    analyzing organisms on the right

--- Algorithm/Algorithm.py
+++ Algorithm/Algorithm.py
@@ -1,4 +1,5 @@
 from Matrix.Matrix import Matrix
+from Matrix.InternalNode import InternalNode
 
 class Algorithm:
     def __init__(self,matrix):
@@ -7,3 +8,11 @@ class Algorithm:
 
     def clone(self,matrix : Matrix) -> Matrix:
         return matrix.clone()
+    
+    def lastCell_R(self,node : InternalNode, idOrganism):
+        if node != None and node.value != 0 and node.value != idOrganism:
+            if node.right != None and node.right.value != idOrganism:
+                return self.lastCell_R(node.right,idOrganism)
+            else:
+                return InternalNode(node.row, node.column)
+        return None
